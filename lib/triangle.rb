05d1648323a9ae14008b07d0c72e3cadbecd8da0
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-26 21:46:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,20 @@
 class Triangle
   
-  def initialize(side_one:, side_two:, side_three:)
+  attr_accessor :x, :y, :z
+  def initialize(x:, y:, z:)
+    @x = x
+    @y = y
+    @z = z
   end
 
   def kind
   end
 
 end
-  class TriangleError < StandardError
-    def message
-      "Each side must be larger than zero, and the sum of the lengths of any two sides always exceeds the length of the third side."
-    end
+
+class TriangleError < StandardError
+  def message
+    "Each side must be larger than zero, and the sum of the lengths of any two sides always exceeds the length of the third side."
   end
+end
   
