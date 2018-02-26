71220836e8ea298b8f59b92f8868867525004499
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-26 21:59:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,9 @@ class Triangle
   end
 
   def kind
-    if @x =< 0 || @y =< 0 || @z =< 0
+    if @x <= 0 || @y <= 0 || @z <= 0
       raise TriangleError
-    elsif @x + @y =< @z || @x + @z =< @y || @z + @y =< @x
+    elsif @x + @y <= @z || @x + @z <= @y || @z + @y <= @x
       raise TriangleError
     else
       if @x == @y == @z
